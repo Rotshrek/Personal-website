93f7b2020a8081cf3915a095c426fff1f54f3aa8
Andrés Sanhueza <trazo263@gmail.com>
2022-11-16 19:29:17 +0100

    first commit

---
+++ website/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Andrés Sanhueza</title>
+        <meta name="description" content="FrontEnd Developer" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Andrés Sanhueza
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
